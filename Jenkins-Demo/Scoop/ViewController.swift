87e35fad795f9787442210c40317ea7746ffc995
Chirag Kava <chirag.kava@einfochips.com>
2017-09-18 16:38:11 +0530

    two files added commit Scoop

---
+++ Jenkins-Demo/Scoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scoop
+//
+//  Created by einfochips on 28/02/17.
+//  Copyright © 2017 Einfochips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
